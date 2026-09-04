c295c1accb7b69d120876170618348ac10f52473
PavelLizunov <87985653+PavelLizunov@users.noreply.github.com>
2026-09-04 16:45:21 +0000

    perf(xhttp): eliminate http.Cookie heap allocation and fmt.Sprintf in cookie padding

--- transport/v2rayxhttp/xpadding.go
+++ transport/v2rayxhttp/xpadding.go
@@ -73,7 +73,12 @@ func (c *Client) applyXPadding(request *http.Request) {
 	}
 	switch m.xPaddingPlacement {
 	case placementCookie:
-		request.AddCookie(&http.Cookie{Name: m.xPaddingKey, Value: pad, Path: "/"})
+		s := m.xPaddingKey + "=" + pad
+		if cur := request.Header.Get("Cookie"); cur != "" {
+			request.Header.Set("Cookie", cur+"; "+s)
+		} else {
+			request.Header.Set("Cookie", s)
+		}
 	case placementHeader:
 		request.Header.Set(m.xPaddingHeader, pad)
 	case placementQuery:
